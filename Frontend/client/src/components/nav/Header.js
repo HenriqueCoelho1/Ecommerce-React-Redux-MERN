34435bd2224c225b6338b734befdafc738c2542e
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-02-05 01:01:45 -0300

    Firebase and register

--- Frontend/client/src/components/nav/Header.js
+++ Frontend/client/src/components/nav/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Menu } from "antd";
 import { AppstoreOutlined, SettingOutlined, UserOutlined, UserAddOutlined } from "@ant-design/icons";
+import {Link} from 'react-router-dom'
 
 const { SubMenu, Item } = Menu;
 
@@ -16,7 +17,7 @@ const Header = () => {
   return (
     <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
         <Item key="home" icon={<AppstoreOutlined />}>
-          Home
+          <Link to="/">Home</Link>
         </Item>
 
         <Item key="register" 
@@ -24,14 +25,14 @@ const Header = () => {
         className="float-right"
         
         >
-          Register
+          <Link to="/register">Register</Link>
         </Item>
 
 
         <Item key="login" 
         icon={<UserOutlined />} 
         className="float-right">
-          Login
+          <Link to="/login">Login</Link>
         </Item>
 
         
