d9a06f3f27724ad9b613917aad00c5c1f664a354
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-02-05 18:44:56 -0300

    Adicionando notifacoes pelo .env

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -1,14 +1,19 @@
 import React, { Fragment } from 'react'
 import {Switch, Route} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
 import Home from './pages/Home'
 import Header from './components/nav/Header'
 
+
 const App = ()  => {
   return (
     <Fragment>
       <Header/>
+      <ToastContainer/>
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/login" component={Login}/>
