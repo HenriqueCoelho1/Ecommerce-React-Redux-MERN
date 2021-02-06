cc65e72c1ae6ce7c05e0ca196776d2b9e91903ae
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-02-06 01:48:39 -0300

    Adicionando o token

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { useEffect, Fragment } from 'react'
 import {Switch, Route} from 'react-router-dom'
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -10,7 +10,33 @@ import Header from './components/nav/Header'
 import RegisterComplete from './pages/auth/RegisterComplete'
 
 
+import {auth} from './firebase'
+import {useDispatch} from 'react-redux'
+
+
 const App = ()  => {
+  const dispatch = useDispatch()
+
+  //to check firebase auth state
+  useEffect(()=> {
+    const unsubscribe = auth.onAuthStateChanged(async (user) =>{
+      if(user){
+        const idTokenResult = await user.getIdTokenResult()
+        console.log("user", user)
+        dispatch({
+          type: 'LOGGED_IN_USER',
+          payload: {
+            email: user.email,
+            token: idTokenResult.token
+          }
+        })
+      }
+    })
+
+    //cleanup
+    return()=> unsubscribe()
+
+  }, [])
   return (
     <Fragment>
       <Header/>
