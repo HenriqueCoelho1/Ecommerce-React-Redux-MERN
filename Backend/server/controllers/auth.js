a75780cc2b95e906b5ff7677d43666f9e5512db1
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-02-08 22:41:35 -0300

    Create and update user

--- Backend/server/controllers/auth.js
+++ Backend/server/controllers/auth.js
@@ -1,5 +1,23 @@
-exports.createOrUpdateUser = (req, res) => {
-    res.json({
-        data: 'Hey you hit the create-or-update-user endpoint'
-    })
+const User = require('../models/user')
+exports.createOrUpdateUser = async (req, res) => {
+    const {name, picture, email} = req.user
+
+    const user = await User.findOneAndUpdate(
+        {email: email}, 
+        {name: name}, 
+        {new: true}
+        )
+
+    if(user){
+        console.log('User updated', user)
+        res.json(user)
+    }else{
+        const newUser = await new User({
+            email,
+            name,
+            picture
+        }).save()
+        console.log('User created', newUser)
+        res.json(newUser)
+    }
 }
