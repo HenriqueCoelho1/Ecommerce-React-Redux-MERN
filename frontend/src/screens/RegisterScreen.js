10a42767f8f250bdcaedb1ff9ce95269d6f51aa2
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-03-30 02:43:37 -0300

    Arrumando

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -12,7 +12,7 @@ const RegisterScreen = () => {
 
 
 
-    const [email, setEmail] = useState('')
+    const [email, setEmail] = useState('megawats')
     return (
         <Container className='p-5'>
             <Row>
